96d28f5ad6ce9e3da06ff8b97b13b4d585fab3c4
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 22:58:44 -0700

    shorten variable names

--- sparkl.c
+++ sparkl.c
@@ -2,13 +2,13 @@ float f(char *s) {
         return strtof(s, 0);
 }
 int main(int c, char **v) {
-        int min = 1, max = 1, i;
+        int n = 1, x = 1, i;
         for (i = 1; i < c; ++i) {
-                if (f(v[i]) < f(v[min]))
-                        min = i;
-                if (f(v[i]) > f(v[max]))
-                        max = i;
+                if (f(v[i]) < f(v[n]))
+                        n = i;
+                if (f(v[i]) > f(v[x]))
+                        x = i;
         }
         for (i = 1; i < c; ++i)
-                printf("%c%c%c%.*s", 226, 150, 129 + (int)((f(v[i]) - f(v[min])) / ((f(v[max]) - f(v[min])) / 8)), i+1 == c, "\n");
+                printf("%c%c%c%.*s", 226, 150, 129 + (int)((f(v[i]) - f(v[n])) / ((f(v[x]) - f(v[n])) / 8)), i+1 == c, "\n");
 }
