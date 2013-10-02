e37aeda44433bc0b5be829abb81c7f49fd7ba5dc
J. David Lowe <j.david.lowe@gmail.com>
2013-10-02 15:19:38 -0700

    eliminate cast

--- sparkl.c
+++ sparkl.c
@@ -1,5 +1,5 @@
 main(c,v)char**v;{
-    char n=1,x=n,i=c-x;
+    char n=1,x=n,i=c-x,q;
     for (;i;i--) f(i) < f(n) ? n = i : f(i) > f(x) ? x = i : i;
-    while (++i < c) printf("\xe2\x96%c", 129 + (int)(8 * (f(i) - f(n)) / (f(x) - f(n))));
+    while (++i < c) printf("\xe2\x96%c", 129 + (q = 8 * (f(i) - f(n)) / (f(x) - f(n))));
 }
