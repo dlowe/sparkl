60a8445d92ad7a08beb8fe6f16de51ee3e2efec1
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 22:51:25 -0700

    get rid of x

--- sparkl.c
+++ sparkl.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main(int c, char **v) {
-        int min = 1, max = 1;
-        for (int i = 1; i < c; ++i) {
+        int min = 1, max = 1, i;
+        for (i = 1; i < c; ++i) {
                 if (strtof(v[i], 0) < strtof(v[min], 0)) {
                         min = i;
                 }
@@ -11,8 +11,7 @@ int main(int c, char **v) {
                         max = i;
                 }
         }
-        for (int i = 1; i < c; ++i) {
-                int x = ((strtof(v[i], 0) - strtof(v[min], 0)) / ((strtof(v[max], 0) - strtof(v[min], 0)) / 8));
-                printf("%c%c%c%.*s", 226, 150, 129 + x, i+1 == c, "\n");
+        for (i = 1; i < c; ++i) {
+                printf("%c%c%c%.*s", 226, 150, 129 + (int)((strtof(v[i], 0) - strtof(v[min], 0)) / ((strtof(v[max], 0) - strtof(v[min], 0)) / 8)), i+1 == c, "\n");
         }
 }
