07b54732990eadc0c5c53a49e63eba5c2ca61188
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 23:47:36 -0700

    chars will do

--- sparkl.c
+++ sparkl.c
@@ -1,15 +1,15 @@
-float f(char *s) {
-        return strtof(s, 0);
+char **V, n, x, i;
+float f(s) {
+        return strtof(V[s], 0);
 }
 main(int c, char **v) {
-        int n, x, i;
+        V=v;
         for (i = c - (n=x=1); i; --i) {
-                if (f(v[i]) < f(v[n]))
+                if (f(i) < f(n))
                         n = i;
-                if (f(v[i]) > f(v[x]))
+                if (f(i) > f(x))
                         x = i;
         }
-        for (i++; i < c; ++i) {
-                printf("\xe2\x96%c%.*s", 129 + (int)((f(v[i]) - f(v[n])) / ((f(v[x]) - f(v[n])) / 8)), i+1 == c, "\n");
-        }
+        for (i++; i < c; ++i)
+                printf("\xe2\x96%c%.*s", 129 + (int)((f(i) - f(n)) / ((f(x) - f(n)) / 8)), i+1 == c, "\n");
 }
