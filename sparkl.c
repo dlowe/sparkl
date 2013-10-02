b21f5dc62d250404c095c8eacadcabfb25a8aa99
J. David Lowe <j.david.lowe@gmail.com>
2013-10-02 14:59:24 -0700

    evil

--- sparkl.c
+++ sparkl.c
@@ -1,7 +1,7 @@
 main(c,v)char**v;{
         char n,x,i;
-        for (i = c - (n=x=1); i; --i)
-                f(i) < f(n) ? n = i : f(i) > f(x) ? x = i : i;
+        for (i = c - (n=x=1); i;)
+                f(i) < f(n) ? n = i-- : f(i) > f(x) ? x = i-- : i--;
         for (i++;i<c;)
                 printf("\xe2\x96%c", 129 + (int)(8 * (f(i++) - f(n)) / (f(x) - f(n))));
 }
