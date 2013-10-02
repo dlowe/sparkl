ee3c067c864c746e017a37b299045b9276ae8fcc
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 23:49:44 -0700

    compress the first loop

--- sparkl.c
+++ sparkl.c
@@ -4,12 +4,9 @@ float f(s) {
 }
 main(int c, char **v) {
         V=v;
-        for (i = c - (n=x=1); i; --i) {
-                if (f(i) < f(n))
-                        n = i;
-                if (f(i) > f(x))
-                        x = i;
-        }
+        for (i = c - (n=x=1); i; --i)
+                f(i) < f(n) && (n = i),
+                f(i) > f(x) && (x = i);
         for (i++; i < c; ++i)
                 printf("\xe2\x96%c%.*s", 129 + (int)((f(i) - f(n)) / ((f(x) - f(n)) / 8)), i+1 == c, "\n");
 }
