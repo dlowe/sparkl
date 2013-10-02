3f4e2409ae9f718a5f0c577b54bafd98cdd6c77b
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 23:41:34 -0700

    consolidate assignments

--- sparkl.c
+++ sparkl.c
@@ -2,13 +2,14 @@ float f(char *s) {
         return strtof(s, 0);
 }
 main(int c, char **v) {
-        int n = 1, x = 1, i;
-        for (i = 2; i < c; ++i) {
+        int n, x, i;
+        for (i = c - (n=x=1); i; --i) {
                 if (f(v[i]) < f(v[n]))
                         n = i;
                 if (f(v[i]) > f(v[x]))
                         x = i;
         }
-        for (i = 1; i < c; ++i)
+        for (i++; i < c; ++i) {
                 printf("\xe2\x96%c%.*s", 129 + (int)((f(v[i]) - f(v[n])) / ((f(v[x]) - f(v[n])) / 8)), i+1 == c, "\n");
+        }
 }
