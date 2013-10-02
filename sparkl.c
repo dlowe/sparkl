438ccf511c5734e7d071c3666005336a62b79446
J. David Lowe <j.david.lowe@gmail.com>
2013-10-02 00:02:08 -0700

    remove whitespace to see what it looks like

--- sparkl.c
+++ sparkl.c
@@ -1,11 +1,7 @@
-char **V, n, x, i;
-float f(s) {
-        return strtof(V[s], 0);
-}
+char**V,n,x,i;
+float f(s){return strtof(V[s],0);}
 main(int c, char **v) {
-        for (V=v, i = c - (n=x=1); i; --i)
-                f(i) < f(n) && (n = i),
-                f(i) > f(x) && (x = i);
+        for(V=v,i=c-(n=x=1);i;--i)f(i)<f(n)&&(n=i),f(i)>f(x)&&(x=i);
         for (i++; i < c;)
                 printf("\xe2\x96%c%.*s", 129 + (int)((f(i) - f(n)) / ((f(x) - f(n)) / 8)), i++ == c, "\n");
 }
