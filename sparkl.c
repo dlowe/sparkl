543619126c06e1fdce2ade22b7d3a8d2c80558da
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 23:52:05 -0700

    tighten loops some more

--- sparkl.c
+++ sparkl.c
@@ -3,10 +3,9 @@ float f(s) {
         return strtof(V[s], 0);
 }
 main(int c, char **v) {
-        V=v;
-        for (i = c - (n=x=1); i; --i)
+        for (V=v, i = c - (n=x=1); i; --i)
                 f(i) < f(n) && (n = i),
                 f(i) > f(x) && (x = i);
-        for (i++; i < c; ++i)
-                printf("\xe2\x96%c%.*s", 129 + (int)((f(i) - f(n)) / ((f(x) - f(n)) / 8)), i+1 == c, "\n");
+        for (i++; i < c;)
+                printf("\xe2\x96%c%.*s", 129 + (int)((f(i) - f(n)) / ((f(x) - f(n)) / 8)), i++ == c, "\n");
 }
