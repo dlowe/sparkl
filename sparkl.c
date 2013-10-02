806624f88d41e2176a757b32bfd5ecfca6960274
J. David Lowe <j.david.lowe@gmail.com>
2013-10-02 15:54:11 -0700

    7

--- sparkl.c
+++ sparkl.c
@@ -1,5 +1,5 @@
 main(c,v)char**v;{
     char n=1,x=n,i=c-x;
-    for (;i;i--) _(i) < _(n) ? n = i : _(i) > _(x) ? x = i : i;
+    for (;i;i--) _(i) < _(n) ? n = i : _(i) > _(x) ? x = i : 7;
     while (++i < c) printf("\xe2\x96%c", 129 + (**v = 8 * (_(i) - _(n)) / (_(x) - _(n))));
 }
