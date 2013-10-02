5e7a450fc95233dc22138a81727624ed9f7ed35d
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 22:52:00 -0700

    get rid of needless braces

--- sparkl.c
+++ sparkl.c
@@ -4,14 +4,11 @@
 int main(int c, char **v) {
         int min = 1, max = 1, i;
         for (i = 1; i < c; ++i) {
-                if (strtof(v[i], 0) < strtof(v[min], 0)) {
+                if (strtof(v[i], 0) < strtof(v[min], 0))
                         min = i;
-                }
-                if (strtof(v[i], 0) > strtof(v[max], 0)) {
+                if (strtof(v[i], 0) > strtof(v[max], 0))
                         max = i;
-                }
         }
-        for (i = 1; i < c; ++i) {
+        for (i = 1; i < c; ++i)
                 printf("%c%c%c%.*s", 226, 150, 129 + (int)((strtof(v[i], 0) - strtof(v[min], 0)) / ((strtof(v[max], 0) - strtof(v[min], 0)) / 8)), i+1 == c, "\n");
-        }
 }
