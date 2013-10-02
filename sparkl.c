b6a316c9e12466a88ecfea4c145bc6b87c90f0b8
J. David Lowe <j.david.lowe@gmail.com>
2013-10-02 15:06:05 -0700

    shorten again

--- sparkl.c
+++ sparkl.c
@@ -1,7 +1,5 @@
 main(c,v)char**v;{
-        char n,x,i;
-        i = c - (n = x = 1);
-        while (i) f(i) < f(n) ? n = i-- : f(i) > f(x) ? x = i-- : i--;
-        ++i;
-        while (i < c) printf("\xe2\x96%c", 129 + (int)(8 * (f(i++) - f(n)) / (f(x) - f(n))));
+    char n=1,x=n,i=c-n;
+    while (i) f(i) < f(n) ? n = i-- : f(i) > f(x) ? x = i-- : i--;
+    while (++i < c) printf("\xe2\x96%c", 129 + (int)(8 * (f(i) - f(n)) / (f(x) - f(n))));
 }
