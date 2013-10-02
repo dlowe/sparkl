c0a5eedd68cc194a945751fc9dae4db2d90dc081
J. David Lowe <j.david.lowe@gmail.com>
2013-10-01 23:15:34 -0700

    warnings are okay

--- sparkl.c
+++ sparkl.c
@@ -1,9 +1,9 @@
 float f(char *s) {
         return strtof(s, 0);
 }
-int main(int c, char **v) {
+main(int c, char **v) {
         int n = 1, x = 1, i;
-        for (i = 1; i < c; ++i) {
+        for (i = 2; i < c; ++i) {
                 if (f(v[i]) < f(v[n]))
                         n = i;
                 if (f(v[i]) > f(v[x]))
