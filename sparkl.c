419be03b9dc8447275916d1fba939ef2f328417e
J. David Lowe <j.david.lowe@gmail.com>
2013-10-02 14:56:29 -0700

    further compression via ternary operator

--- sparkl.c
+++ sparkl.c
@@ -1,8 +1,7 @@
 main(c,v)char**v;{
         char n,x,i;
         for (i = c - (n=x=1); i; --i)
-                f(i) < f(n) && (n = i),
-                f(i) > f(x) && (x = i);
-        for (i++; i < c;)
-                printf("\xe2\x96%c%.*s", 129 + (int)(8 * (f(i) - f(n)) / (f(x) - f(n))), i++ == c, "\n");
+                f(i) < f(n) ? n = i : f(i) > f(x) ? x = i : i;
+        for (i++;i<c;)
+                printf("\xe2\x96%c", 129 + (int)(8 * (f(i++) - f(n)) / (f(x) - f(n))));
 }
